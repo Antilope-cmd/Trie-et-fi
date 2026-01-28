64edc87754bbead9689033dc9c8cf66a5829e995
Antilope-cmd <assadnorat01@gmail.com>
2026-01-28 17:06:46 +0100

    Devolopping shuffle button

--- source/main.py
+++ source/main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from classes import *
-from random import randint
+from random import randint, shuffle
 
 WINDOW_COEFF = 0.5
 WINDOW_HEIGHT = 1080
@@ -16,23 +16,29 @@ root.grid_rowconfigure(0, weight=1)
 
 
 canvas = tk.Canvas(root, background="red")
-Interface = tk.Frame(root, background="blue")
+interface = tk.Frame(root, background="blue")
 
 canvas.grid(column=0, row=0, sticky="nsew")
-Interface.grid(column=1, row=0, sticky="nsew")
+interface.grid(column=1, row=0, sticky="nsew")
 
 HEIGHT_CANVAS = canvas.winfo_height()
 WIDTH_CANVAS = canvas.winfo_width()
 
+main_list = [Histogram(randint(30, 500), canvas, width=20) for i in range(30)]
 
 
 """HERE GOES THE BUTTONS OF THE INTERFACE"""
+def shuffle_mainlist() -> None:
+    global main_list
+    main_list = shuffle(main_list)
+    return
+randomise_button = tk.Button(interface, text="Mélanger", command=shuffle_mainlist)
 
+randomise_button.pack()
 
 
 
 """HERE GOES THE LOGIC TO REPRESENT A LIST OF NUMBERS"""
-main_list = [Histogram(randint(30, 500), canvas, width=20) for i in range(30)]
 def canvas_mainloop():
     canvas.delete("all")    
     for index, histogram in enumerate(main_list):
