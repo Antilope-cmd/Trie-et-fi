ef2c36c0c74af9d895eb7fbdb59924a64b14ef66
Antilope-cmd <assadnorat01@gmail.com>
2026-02-22 16:52:56 +0100

    removed unused lines and added proportional scaling
    The Histograms will now take up exactly the width of the canvas window.

--- source/classes.py
+++ source/classes.py
@@ -15,25 +15,30 @@ class Histogram():
     def get_dimensions(self):
         return self.canvas.winfo_height(), self.canvas.winfo_width()
 
-    def draw(self, position:int, spacing:int):
+    def draw(self, position:int, hist_amount:int):
         """Draws the histogram on the canvas. Only to be used at initialisation."""
         self.previous_position = position
+        
         canvas_height, canvas_width = self.get_dimensions()
-        self.x1 = position*(spacing+self.width) + 10
+
+        self.width = (canvas_width-10)/hist_amount
+
+        self.x1 = position*(self.width) + 10
         self.y1 = canvas_height
         self.x2 = self.x1 + self.width
         self.y2 = canvas_height - self.height
-        self.canvas_id = self.canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, fill=self.colour)
+        self.canvas_id = self.canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, fill=self.colour, outline='')
         return
     
-    def update_coords(self, position:int, spacing, force_update=False):
+    def update_coords(self, position:int, hist_amount:int, force_update=False):
 
         if position == self.previous_position and not force_update:
             return  #If position in the list didn't change no need to update the coordinates
         
         self.previous_position = position
         canvas_height, canvas_width = self.get_dimensions()
-        self.x1 = position*(spacing+self.width) + 10
+        self.width = (canvas_width-10)/hist_amount
+        self.x1 = position*(self.width) + 10
         self.y1 = canvas_height
         self.x2 = self.x1 + self.width
         self.y2 = canvas_height - self.height
@@ -44,6 +49,3 @@ class Histogram():
         self.colour = colour
         self.canvas.itemconfig(self.canvas_id, fill=colour)
         return
-
-    def get_value(self):
-        self.change_color("red")
