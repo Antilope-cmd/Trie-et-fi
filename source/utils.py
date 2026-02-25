3ed4edd0d39db8b29ebc488d363dcc0bf09a0c4a
Antilope-cmd <assadnorat01@gmail.com>
2026-02-25 23:59:19 +0100

    Migrated functions
    Update canvas display will only update the coordinates of the histograms that were modified

--- source/utils.py
+++ source/utils.py
@@ -1,18 +1,10 @@
 import tkinter as tk
 from classes import Histogram
+from random import shuffle
 
 def get_dimensions(canvas:tk.Canvas):
     """returns Height/width"""
     return  canvas.winfo_height(), canvas.winfo_width()
-
-
-def fill_percentage(canvas:tk.Canvas, percentage_x, percentage_y):
-    height, width = get_dimensions(canvas)
-
-    height_final = height*percentage_x/100
-    width_final = width*percentage_y/100
-
-    return height_final, width_final
 
 def swap_values(main_list, index1, index2):
     color = 'blue'
@@ -22,9 +14,38 @@ def swap_values(main_list, index1, index2):
     return color  #TODO: NOT HERE BUT MAKE A FUNTION TO TURN BACK EVERYTHING WHITE WHILE STILL KEEPING A DELAY
             #NOTE: Maybe add a list of indexes that have their color changed so that we can turn them back white with O(1) efficiency.
 
-def update_canvas_display(main_list:list[Histogram], force_update=False):
+def update_canvas_display(main_list:list[Histogram], pending_updates_list:list[int]=[], force_update=False):
     """Refreshes the coordinates of the histogram according to the window"""
 
+    n = len(main_list)
+
+    if pending_updates_list:
+        for index in pending_updates_list:
+            main_list[index].update_coords(position=index, hist_amount=n, force_update=False)
+        return
+
+
     for index, histogram in enumerate(main_list):
-        histogram.update_coords(hist_amount=len(main_list), position=index, force_update=force_update)
+        histogram.update_coords(hist_amount=n, position=index, force_update=force_update)
+    return
+
+def erase_colors(colored_dict, hist_list):
+
+    for hist in hist_list:
+        hist.change_color("white")
+
+    for color in colored_dict:
+        colored_dict[color].clear()
+
+def make_listbox(master, sorts:dict):
+    listbox = tk.Listbox(master)
+    for sort_name in sorts.keys():
+        listbox.insert(0, sort_name)
+    return listbox
+
+
+def shuffle_mainlist(mainlist, colored_dict) -> None:
+    erase_colors(colored_dict=colored_dict, hist_list=mainlist)
+    shuffle(mainlist)
+    update_canvas_display(main_list=mainlist, force_update=True)
     return
